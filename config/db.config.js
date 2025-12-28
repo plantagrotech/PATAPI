0c800cd7e0e447d06c0dbf4ae9f317af373e841d
Sudhir <plantagrotech@gmail.com>
2025-12-28 22:27:12 +0530

    local testing

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,20 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "admin@123",
-  DB: "plantagrotech",
+  // HOST: "localhost",
+  // USER: "postgres",
+  // PASSWORD: "admin@123",
+  // DB: "plantagrotech",
+  // dialect: "postgres",
+  // pool: {
+  //   max: 5,
+  //   min: 0,
+  //   acquire: 30000,
+  //   idle: 10000
+  // }
+
+ HOST: "db-postgresql-blr1-85141-do-user-30830133-0.i.db.ondigitalocean.com",
+  USER: "doadmin",
+  PASSWORD: "AVNS_xAsh6QWfFv-DnrS_3jn",
+  DB: "defaultdb",
   dialect: "postgres",
   pool: {
     max: 5,
@@ -10,6 +22,5 @@ module.exports = {
     acquire: 30000,
     idle: 10000
   }
-
   
 };
