6b2bc4db4dac379e7431af919780ee5b146099eb
Sudhir <plantagrotech@gmail.com>
2025-12-28 22:22:43 +0530

    local testing

--- config/db.config.js
+++ config/db.config.js
@@ -11,15 +11,5 @@ module.exports = {
     idle: 10000
   }
 
-  // HOST: "database-1.cdw06eysqxxo.ap-south-1.rds.amazonaws.com",
-  // USER: "postgres",
-  // PASSWORD: "Japkaran2025",
-  // DB: "plantagrotech",
-  // dialect: "postgres",
-  // pool: {
-  //   max: 5,
-  //   min: 0,
-  //   acquire: 30000,
-  //   idle: 10000
-  // }
+  
 };
